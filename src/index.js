8ebb61cc8bd3708cbd97106431ea080da7aac14a
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-14 19:51:58 -0300

    Create Project and Config Routes

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Routes from './routes';
+
+const App = () => <Routes />;
+/*
+    código acima é igual a este:
+    class App extends Component {
+        render(){
+            return <Routes />
+        }
+    }
+*/
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import { createStackNavigator } from 'react-navigation'; // navegação por botão. Tem em abas, menu laterais etc...
+import Main from './pages/main';
+
+export default createStackNavigator({
+    Main,
+});
