c8b84d3b6c345c72ef65ff3b3d281bccda3b080e
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-15 00:48:58 -0300

    Adding ReactNavigation title

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,6 +3,10 @@ import { View, Text } from 'react-native'
 
 export default class Main extends Component {
 
+    static navigationOptions = {
+        title: 'JSHunt'
+    };
+
     render() {
         return (
             <View>
