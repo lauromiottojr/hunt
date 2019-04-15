361885aa058ff38ccff1a64cf8e3126be81c97b7
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-15 01:34:46 -0300

    Getting API

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native'
+import { View, Text } from 'react-native';
+import api from '../services/api';
 
 export default class Main extends Component {
 
     static navigationOptions = {
         title: 'JSHunt'
     };
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => { // sem o arrowFunction, ele não consegue enxergar o this, assim não consigo acessar outros metodos da classe
+        const response = await api.get('/products');
+        const { docs } = response.data;
+        console.log(docs);
+    };
+    
 
     render() {
         return (
