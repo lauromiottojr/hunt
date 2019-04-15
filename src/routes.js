68a78c980623106cbdb47de77658932716ca521f
Lauro Miotto Junior <junior.miotto36@gmail.com>
2019-04-15 02:52:05 -0300

    Access All Products

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { createStackNavigator } from 'react-navigation'; // navegação por botão. Tem em abas, menu laterais etc...
 import Main from './pages/main';
+import Product from './pages/product';
 
 export default createStackNavigator({
     Main,
+    Product
 }, {
         navigationOptions: {
             headerStyle: {
